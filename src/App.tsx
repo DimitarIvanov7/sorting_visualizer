9aa2784d8cff77d11350eadd9f18ba5ff694c5e6
Dimitar <dimiturivanov92@gmail.com>
2022-03-23 10:36:00 +0200

    eslint check

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,5 @@
 import React from "react";
 
-const testFunc = (test) => {
-  debugger;
-  !!!!console.log("test");
-};
-
 function App() {
   return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
 }
--- src/test.js
+++
@@ -1,3 +0,0 @@
-let penis = "penis";
-
-// debugger;
